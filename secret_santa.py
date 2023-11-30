a36c535d7fca84ce809f1d6bf7331f17b6abb2a5
Reed Taylor <reed.taylor@me.com>
2023-11-29 16:07:31 -0800

    Add backtracking algorithm that uses recursion

--- secret_santa.py
+++ secret_santa.py
@@ -2,8 +2,13 @@ from abc import ABC, abstractmethod
 
 class SantaBase(ABC):
     def __init__(self, participant_couples: list[list[str]], count: int):
+        import random
+        self.random = random
+        self.random.seed()
+
         self.participant_couples = participant_couples
         self.count = count
+        self.all_participants = [item for sublist in self.participant_couples for item in sublist]
         self.assignments : dict[str, list[str]] = {}
 
     @abstractmethod
@@ -26,9 +31,7 @@ class SantaBase(ABC):
             print()
 
     def _get_possible_assignments(self, participant: str, assignments: dict[str, list[str]]) -> list[str]:
-        all_participants = [item for sublist in self.participant_couples for item in sublist]
-
-        return [item for item in all_participants if item not in self._get_disallowed_assignments(participant, assignments)]
+        return [item for item in self.all_participants if item not in self._get_disallowed_assignments(participant, assignments)]
 
 
     def _get_disallowed_assignments(self, participant: str, assignments: dict[str, list[str]]) -> set[str]:
@@ -44,7 +47,6 @@ class SantaBase(ABC):
         disallowed_assignments.extend(current_assignments)
 
         return set(disallowed_assignments)
-
 
 class BruteForceSanta(SantaBase):
     def __init__(self, participant_couples: list[list[str]], count: int):
@@ -70,10 +72,6 @@ class BruteForceSanta(SantaBase):
                 print('Failed to assign secret santas. Trying again...')
 
     def _attempt_assign(self) -> bool:
-        from random import seed
-        from random import randint
-        seed()
-
         for couple in self.participant_couples:
             for participant in couple:
                 possible_assignments = self._get_possible_assignments(participant, self.assignments)
@@ -81,7 +79,7 @@ class BruteForceSanta(SantaBase):
                 if len(possible_assignments) == 0:
                     return False
 
-                newAssignment = possible_assignments[randint(0, len(possible_assignments) - 1)]
+                newAssignment = possible_assignments[self.random.randint(0, len(possible_assignments) - 1)]
 
                 if self.assignments.get(participant) is None:
                     self.assignments[participant] = [newAssignment]
@@ -90,31 +88,56 @@ class BruteForceSanta(SantaBase):
 
         return True
 
-
-class CompleteSanta(SantaBase):
+class BacktrackingSanta(SantaBase):
     def __init__(self, participant_couples: list[list[str]], count: int):
         super().__init__(participant_couples, count)
 
     def _get_possible_assignment(self):
-        from random import seed
-        from random import randint
-        seed()
+        participants : list[str] = []
 
-        all_possible = self._get_all_possible_assignments()
-        self.assignments = all_possible[randint(0, len(all_possible) - 1)]
+        for _ in range(self.count):
+            participants.extend(self.all_participants.copy())
 
-    def _get_all_possible_assignments(self) -> list[dict[str, list[str]]]:
-        all_possible = []
+        self.random.shuffle(participants)
 
-        # TODO
+        if (len(participants) != len(self.all_participants * count)):
+            print('Failed to generate participants...')
 
-        return all_possible
+        if not self._recursive_solve(participants):
+            print('Failed to find a solution...')
 
+    def _recursive_solve(self, remaining_participants: list[str]) -> bool:
+        if len(remaining_participants) == 0:
+            return True
+
+        participant = remaining_participants[0]
+        possible_assignments = self._get_possible_assignments(participant, self.assignments).copy()
+
+        if len(possible_assignments) == 0:
+            return False
+
+        self.random.shuffle(possible_assignments)
+
+        for assignment in possible_assignments:
+            if self.assignments.get(participant) is None:
+                self.assignments[participant] = [assignment]
+            else:
+                self.assignments[participant].append(assignment)
+
+            if self._recursive_solve(remaining_participants[1:]):
+                return True
+
+            if len(self.assignments[participant]) == 1:
+                del self.assignments[participant]
+            else:
+                self.assignments[participant].remove(assignment)
+
+        return False
 
 # Run
-couples = [['Reed', 'Brooke'], ['Lacey', 'Bryce'], ['Darcy', 'Jon']]
+couples = [['Mike', 'Lorie'], ['Reed', 'Brooke'], ['Lacey', 'Bryce'], ['Darcy', 'Jon']]
 count = 2
-use_brute_force = True
+use_brute_force = False
 
-santa_assigner : SantaBase = BruteForceSanta(couples, count) if use_brute_force else CompleteSanta(couples, count)
+santa_assigner : SantaBase = BruteForceSanta(couples, count) if use_brute_force else BacktrackingSanta(couples, count)
 santa_assigner.generate_assignments()
